5e3d35b8ae6cca147ec16888dd6e9a2cb194cb25
Igor Santos <falaigors@gmail.com>
2021-01-10 02:05:22 -0300

    Add Home page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,33 @@
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import Header from './src/Header';
+import Home from './src/Home';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Montserrat_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Header />
+      <Home />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
